922d302b192e56cf33e2b3ab8b737905442b62a3
evansmwenda <evansmwenda.em@gmail.com>
2020-10-05 15:39:55 +0300

    working on the navigation menu

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Homepage extends StatefulWidget {
   static const routeName = '/homepage';
   @override
@@ -14,6 +15,36 @@ class _State extends State<Homepage> {
       ),
       body: Center(
         child: Text("Home page"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
